70d93044468d2e0fbada261e9c5cd0dd627a1764
lk <1573002354@qq.com>
2026-04-23 17:32:22 +0800

    AI生成应用-门面设计模式

---
+++ backend/src/main/java/com/lk/aizerocodeplatform/core/AiCodeGenFacade.java
@@ -0,0 +1,67 @@
+package com.lk.aizerocodeplatform.core;
+
+import com.lk.aizerocodeplatform.ai.AiCodeGenService;
+import com.lk.aizerocodeplatform.ai.model.HtmlCodeResult;
+import com.lk.aizerocodeplatform.ai.model.MultiFileCodeResult;
+import com.lk.aizerocodeplatform.enums.CodeGenTypeEnum;
+import com.lk.aizerocodeplatform.exception.BusinessException;
+import com.lk.aizerocodeplatform.exception.ErrorCode;
+import com.lk.aizerocodeplatform.exception.ThrowUtils;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+
+/**
+ * @Author 梁科
+ * @Version 1.0
+ * @ Date 2026/4/23 17:08
+ * AI代码生成门面类（采用门面设计模式）
+ */
+@Service
+public class AiCodeGenFacade {
+    @Resource
+    private AiCodeGenService aiCodeGenService;
+
+    /**
+     * 生成代码并保存
+     *
+     * @param userMessage     用户提示词
+     * @param codeGenTypeEnum 代码生成类型
+     * @return 文件
+     */
+    public File generateCodeAndSave(String userMessage, CodeGenTypeEnum codeGenTypeEnum) {
+        ThrowUtils.throwIf(codeGenTypeEnum == null, ErrorCode.PARAMS_ERROR);
+        ThrowUtils.throwIf(userMessage == null, ErrorCode.PARAMS_ERROR);
+        return switch (codeGenTypeEnum) {
+            case HTML -> generateCodeHtmlAndSave(userMessage);
+            case MULTI_FILE -> generateCodeMultiFileAndSave(userMessage);
+            default -> {
+                String errorMessage = "不支持的生成类型：" + codeGenTypeEnum.getValue();
+                throw new BusinessException(ErrorCode.SYSTEM_ERROR, errorMessage);
+            }
+        };
+    }
+
+    /**
+     * 保存HTML模式的代码文件
+     *
+     * @param userMessage 用户提示词
+     * @return 文件
+     */
+    private File generateCodeHtmlAndSave(String userMessage) {
+        HtmlCodeResult htmlCodeResult = aiCodeGenService.generateHtmlCode(userMessage);
+        return CodeFileSaver.saveHtml(htmlCodeResult);
+    }
+
+    /**
+     * 保存多文件模式的代码文件
+     *
+     * @param userMessage 用户提示词
+     * @return 文件
+     */
+    private File generateCodeMultiFileAndSave(String userMessage) {
+        MultiFileCodeResult multiFileCodeResult = aiCodeGenService.generateMultiFileCode(userMessage);
+        return CodeFileSaver.saveMultiFile(multiFileCodeResult);
+    }
+}
---
+++ backend/src/test/java/com/lk/aizerocodeplatform/core/AiCodeGenFacadeTest.java
@@ -0,0 +1,27 @@
+package com.lk.aizerocodeplatform.core;
+
+import com.lk.aizerocodeplatform.enums.CodeGenTypeEnum;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @Author 梁科
+ * @Version 1.0
+ * @ Date 2026/4/23 17:27
+ */
+@SpringBootTest
+class AiCodeGenFacadeTest {
+    @Resource
+    private AiCodeGenFacade aiCodeGenFacade;
+    @Test
+    void generateCodeAndSave() {
+        File file = aiCodeGenFacade.generateCodeAndSave("任务记录网站", CodeGenTypeEnum.MULTI_FILE);
+        Assertions.assertNotNull(file);
+    }
+}
